a8a1e877100a3e8bc9c33b67f725cd32f14fdf86
Revilo91 <oliver.suedholt@gmail.com>
2021-02-15 12:20:21 +0100

    formating

--- migrate.py
+++ migrate.py
@@ -27,13 +27,13 @@ except Exception as ex:
     sys.exit(1)
 
 try:
-    MYSQL_CONNECTION = pymysql.connect(host = db['MySQL']['host'],
-                                    port = db['MySQL']['port'],
-                                    user = db['MySQL']['user'],
-                                    password = db['MySQL']['password'],
-                                    db = db['MySQL']['database'])
+    MYSQL_CONNECTION = pymysql.connect(host=db['MySQL']['host'],
+                                       port=db['MySQL']['port'],
+                                       user=db['MySQL']['user'],
+                                       password=db['MySQL']['password'],
+                                       db=db['MySQL']['database'])
 except pymysql.OperationalError as error:
-    print (error)
+    print(error)
     sys.exit(1)
 except Exception as ex:
     print("MySQL connection error")
@@ -41,13 +41,13 @@ except Exception as ex:
     sys.exit(1)
 
 
-INFLUXDB_CONNECTION = InfluxDBClient(host = db['InfluxDB']['host'],
-                                     ssl = db['InfluxDB']['ssl'],
+INFLUXDB_CONNECTION = InfluxDBClient(host=db['InfluxDB']['host'],
+                                     ssl=db['InfluxDB']['ssl'],
                                      verify_ssl=True,
-                                     port = db['InfluxDB']['port'],
-                                     username = db['InfluxDB']['user'],
-                                     password = db['InfluxDB']['password'],
-                                     database = db['InfluxDB']['database'])
+                                     port=db['InfluxDB']['port'],
+                                     username=db['InfluxDB']['user'],
+                                     password=db['InfluxDB']['password'],
+                                     database=db['InfluxDB']['database'])
 
 # Select datapoints
 if len(sys.argv) > 1 and sys.argv[1].upper().strip() == "ALL":
@@ -58,29 +58,29 @@ elif len(sys.argv) == 2:
     print("Migrate '" + sys.argv[1] + "' datapoint(s) ...")
 else:
     print("To migrate all datapoints run '" + sys.argv[0] + " ALL'")
-    print("To migrate one datapoints run '" + sys.argv[0] + " <DATAPONTNAME>'")
+    print("To migrate one datapoints run '" + sys.argv[0] + " <DATAPOINTNAME>'")
     print("To migrate a set of datapoints run '" + sys.argv[0] + ' "hm-rega.0.%"' + "'")
     sys.exit(1)
 print("")
 
 # dictates how columns will be mapped to key/fields in InfluxDB
 SCHEMA = {
-    "time_column": "time", # the column that will be used as the time stamp in influx
-    "columns_to_fields" : ["ack","q", "from","value"], # columns that will map to fields
+    "time_column": "time",  # the column that will be used as the time stamp in influx
+    "columns_to_fields": ["ack", "q", "from", "value"],  # columns that will map to fields
     # "columns_to_tags" : ["",...], # columns that will map to tags
-    "table_name_to_measurement" : "name", # table name that will be mapped to measurement
-    }
+    "table_name_to_measurement": "name",  # table name that will be mapped to measurement
+}
 
 
 #####
-#Generates an collection of influxdb points from the given SQL records
+# Generates an collection of influxdb points from the given SQL records
 #####
 def generate_influx_points(records):
     influx_points = []
     for record in records:
         #tags = {},
         fields = {}
-        #for tag_label in SCHEMA['columns_to_tags']:
+        # for tag_label in SCHEMA['columns_to_tags']:
         #   tags[tag_label] = record[tag_label]
         for field_label in SCHEMA['columns_to_fields']:
             if db['InfluxDB']['store_ack_boolean'] == True:
@@ -91,12 +91,13 @@ def generate_influx_points(records):
 
         influx_points.append({
             "measurement": record[SCHEMA['table_name_to_measurement']],
-            #"tags": tags,
+            # "tags": tags,
             "time": record[SCHEMA['time_column']],
             "fields": fields
         })
 
     return influx_points
+
 
 def query_metrics(table):
     MYSQL_CURSOR.execute("SELECT name, id FROM datapoints WHERE id IN(SELECT DISTINCT id FROM " + table + ")" + MIGRATE_DATAPOINT)
@@ -104,8 +105,9 @@ def query_metrics(table):
     print('Total metrics in ' + table + ": " + str(MYSQL_CURSOR.rowcount))
     return rows
 
+
 def migrate_datapoints(table):
-    query_max_rows = 100000 # prevent run out of mermory limit on SQL DB
+    query_max_rows = 100000  # prevent run out of mermory limit on SQL DB
     process_max_rows = 1000
 
     migrated_datapoints = 0
@@ -115,23 +117,23 @@ def migrate_datapoints(table):
     processed_rows = 0
     for metric in metrics:
         metric_nr += 1
-        print(metric['name'] + "(ID: " + str(metric['id']) + ")" + " (" + str(metric_nr) + "/" + str(metric_count) + ")["+str(round((metric_nr/metric_count)*100,2))+"&]")
+        print(metric['name'] + "(ID: " + str(metric['id']) + ")" + " (" + str(metric_nr) + "/" + str(metric_count) + ")["+str(round((metric_nr/metric_count)*100, 2))+"&]")
 
         start_row = 0
         processed_rows = 0
         while True:
-            query  = """SELECT d.name,
-                                m.ack AS 'ack',
-                                (m.q*1.0) AS 'q',
-                                s.name AS "from",
-                                (m.val*1.0) AS 'value',
-                                (m.ts*1000000) AS'time'
-                                FROM """ + table + """ AS m
-                                LEFT JOIN datapoints AS d ON m.id=d.id
-                                LEFT JOIN sources AS s ON m._from=s.id
-                                WHERE q=0 AND d.id = """ + str(metric['id']) + """
-                                ORDER BY m.ts desc
-                                LIMIT """ + str(start_row)+ """, """ + str(query_max_rows)
+            query = """SELECT d.name,
+                        m.ack AS 'ack',
+                        (m.q*1.0) AS 'q',
+                        s.name AS "from",
+                        (m.val*1.0) AS 'value',
+                        (m.ts*1000000) AS'time'
+                        FROM """ + table + """ AS m
+                        LEFT JOIN datapoints AS d ON m.id=d.id
+                        LEFT JOIN sources AS s ON m._from=s.id
+                        WHERE q=0 AND d.id = """ + str(metric['id']) + """
+                        ORDER BY m.ts desc
+                        LIMIT """ + str(start_row) + """, """ + str(query_max_rows)
             MYSQL_CURSOR.execute(query)
             if MYSQL_CURSOR.rowcount == 0:
                 break
@@ -147,7 +149,7 @@ def migrate_datapoints(table):
                 migrated_datapoints += len(selected_rows)
 
                 try:
-                    INFLUXDB_CONNECTION.write_points(generate_influx_points(selected_rows),retention_policy=db['InfluxDB']['retention_policy'])
+                    INFLUXDB_CONNECTION.write_points(generate_influx_points(selected_rows), retention_policy=db['InfluxDB']['retention_policy'])
                 except Exception as ex:
                     print("InfluxDB error")
                     print(ex)
@@ -159,6 +161,7 @@ def migrate_datapoints(table):
         print("")
 
     return migrated_datapoints
+
 
 MYSQL_CURSOR = MYSQL_CONNECTION.cursor(cursor=pymysql.cursors.DictCursor)
 migrated = 0
